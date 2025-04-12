3f16788905236cae3667b7788f0fe807ebee89dc
Jaime Mendoza <jmendos81@gmail.com>
2025-04-12 21:33:28 +0200

    modificación apiUrl-2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env["NEXT_PUBLIC_API_URL"] || 'http://localhost:3000'
+  apiUrl: process.env["NEXT_PUBLIC_API_URL"] || 'https://gestion-usuarios-backend.fly.dev' // URL del backend en Fly.io
 };
